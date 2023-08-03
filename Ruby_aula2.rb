40139b3d54c661760e14de78c4077f2e8c808db9
ded3v <decolacosta@gmail.com>
2023-08-03 18:40:21 -0300

    Commit das alterações locais

--- Ruby_aula2.rb
+++ Ruby_aula2.rb
@@ -1,5 +1,6 @@
 # chamadas/array
 # usei inicialmente o array: seguradoras = ['porto', 'azul', 'allianz', 'tokio'], mas é recomendando no array de strings usar %w()
+
 seguradoras = %w(porto azul allianz tokio)
 puts "Tipo da variável: #{seguradoras.class}"
 puts "seguradoras do mês de agosto: #{seguradoras[0]}, #{seguradoras[1]}"
@@ -10,11 +11,22 @@ puts clientes_agosto[:cl1]
 
 
 # Operações Básicas
-print 'Agora alguns exemplos de operações com Ruby '
+puts 'Agora alguns exemplos de operações com Ruby '
 print "Digite o primeiro número: "
 number1 = gets.chomp.to_f
 print "Digite o segundo número: "
+
 number2 = gets.chomp.to_f
 
 puts number2*number1
-puts number1 ** numbergit
+puts number1 ** number2
+puts number1 * (number2/number2+number1)
+
+#Programa de entrada/saída
+
+print "Digite seu nome: "
+name = gets.chomp
+print "digite sua idade: "
+idade = gets.chomp
+
+print "Nome do usuário: #{name}, idade: #{idade}"
