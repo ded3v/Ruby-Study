d34a858b926a0107c1e30dac9ba78e1611adc65f
ded3v <decolacosta@gmail.com>
2023-08-04 17:27:22 -0300

    Atualizando algoritmo com loop

--- ruby_aula3.rb
+++ ruby_aula3.rb
@@ -8,19 +8,28 @@ puts "Tipo da variável: #{clientes_agosto.class}"
 puts clientes_agosto[:cl1]
 
 
-puts "Digite a seguradora para saber seus clientes: "
-seg_cliente = gets.chomp
+loop_counter = 0
+while loop_counter < 3
+  puts "Digite a seguradora para saber seus clientes: "
+  seg_cliente = gets.chomp.downcase
 
-if seg_cliente == seguradoras[0]
-  puts "Os clientes deste mês da Porto são: #{clientes_agosto[:cl1]}, #{clientes_agosto[:cl2]}"
-elsif seg_cliente == seguradoras[1]
-  puts "Os clientes deste mês da Azul são: #{clientes_agosto[:cl3]}, #{clientes_agosto[:cl4]}"
-elsif seg_cliente == seguradoras[2]
-  puts "Os clientes deste mês da Azul são: #{clientes_agosto[:cl5]}"
-elsif seg_cliente == seguradoras[3]
-  puts "Não há clientes cadastrados neste seguradora"
-else
-  puts "Seguradora incorreta, digite novamente"
+  if seguradoras.include?(seg_cliente) #Procurar se a entrada corresponde ao dado da array
+    if seg_cliente == seguradoras[0]
+      puts "Os clientes deste mês da Porto são: #{clientes_agosto[:cl1]}, #{clientes_agosto[:cl2]}"
+    elsif seg_cliente == seguradoras[1]
+      puts "Os clientes deste mês da Azul são: #{clientes_agosto[:cl3]}, #{clientes_agosto[:cl4]}"
+    elsif seg_cliente == seguradoras[2]
+      puts "Os clientes deste mês da Azul são: #{clientes_agosto[:cl5]}"
+    elsif seg_cliente == seguradoras[3]
+      puts "Não há clientes cadastrados neste seguradora"
+    end
+
+    break #sai do loop após realizar a condição
+    else
+      puts "Seguradora incorreta, digite novamente"
+      loop_counter += 1
+
+
+  end
 
 end
-
