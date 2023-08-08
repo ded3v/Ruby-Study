ef828023580c82bc13d9dc287b618ab1f964927e
ded3v <decolacosta@gmail.com>
2023-08-07 21:18:01 -0300

    manipulando um array/hash part2

--- ruby_aula4.rb
+++ ruby_aula4.rb
@@ -1,3 +1,5 @@
+#ARRAYS
+
 componentes = %w[memoriaDDR4_8gb ssd500gb RTX3040 monitor144hz ryzen5 motherboardMORTAR545]
 componentes.insert(0, 'computer_case')
 
@@ -20,4 +22,20 @@ puts componentes.empty? #está vazio?
 puts componentes.include?('RTX3040') #Este elemento compoe o array?
 
 #componentes.delete_at(2) ##deletar o elemento da posição 2 do array
+
+#HASH
+
+compp_price = {case: '600', memoria: '300', ssd: '400', RTX: '2400', ryzen5: '800'}
+compp_price[:monitor] = "1000" #adiciona ao hash uma chave e seu valor
+
+puts compp_price
+
+puts compp_price.keys #imprime apenas as chaves
+puts compp_price.values #imprime apenas os valores das chaves
+
+#comp_price.delete(:nome_da_chave) ##exclui a chave
+
+puts compp_price[:memoria] #imprime o valor da chave selecionada
+puts compp_price.size #quantidade de chaves da hash
+
 
