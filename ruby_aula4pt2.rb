cfbb7096ef27ddd6da3ae28aa7f7b10d7f34750e
ded3v <decolacosta@gmail.com>
2023-08-08 12:53:35 -0300

    interações array/hash pt2

--- ruby_aula4pt2.rb
+++ ruby_aula4pt2.rb
@@ -16,3 +16,33 @@ good_habits2.each do |habit, improve|
   puts "#{habit} benefict: #{improve}"
 end
 
+
+#MAP
+
+array = [2, 4, 6, 9] #lembrando que números devem entrar sem '', caso contrário será uma string
+
+new_array = array.map do |a|
+  a * 2
+end
+
+#>>> Para sobrescrever um array existente usa-se a estrutura array.map! do |a|
+
+puts "#{new_array}"
+
+#Escolhendo os elementos da array e manipulando:
+
+selection = new_array.select do |a|
+  a >= 6
+end
+
+puts "#{selection}"
+puts
+
+#Selection no Hash
+
+habits2 = good_habits2.select do |key, value|
+  good_habits2.keys.index(key) > good_habits2.keys.index(:sleep)  #Aqui pegamos todas as chaves da hash e condicionamos para selecionar as chaves após a chave ':sleep'
+end
+
+puts habits2
+
