a892f643489c035880172ae29cb275c781be3976
ded3v <decolacosta@gmail.com>
2023-08-09 12:56:27 -0300

    aula 5/missão especial terminando

--- ruby_aula5.rb
+++ ruby_aula5.rb
@@ -23,9 +23,35 @@ puts
 
 #1 Crie um programa que possua um método que resolva a potência dado um número base e seu expoente. Estes dois valores devem ser informados pelo usuário.
 
+puts "Método 1"
+def calculo_potencia
+  puts "Cálculo de potência"
+  puts "Digite o número base: "
+  x = gets.chomp.to_f
+  puts "Digite a potência: "
+  y = gets.chomp.to_f
+  puts "resultado: #{x**y}"
+end
 
+calculo_potencia
+
+puts
+def calculo_pot(a, b)
+  a**b
+
+end
+
+puts "Método 2"
+puts "Digite o número base: "
+num1 = gets.chomp.to_f
+puts "Digite a potência:"
+num2 = gets.chomp.to_f
+
+puts "Resultado: #{calculo_pot(num1, num2)}"
+puts
 #2 Siga a documentação da gem cpf_cnpj para criar um programa que recebe como entrada um número de cpf e em um método verifique se este número é válido.
 
+puts "Programa para verificar CPF"
 require "cpf_cnpj"
 
 puts "DIgite o cpf para ser veríficado:"
