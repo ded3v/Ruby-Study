e1215869d2855ce6084ca73756164001432f1feb
ded3v <decolacosta@gmail.com>
2023-08-18 19:59:53 -0300

    aula 8 trainning

--- Ruby_Couse_Trainning/ruby_aula8pt2.rb
+++ Ruby_Couse_Trainning/ruby_aula8pt2.rb
@@ -30,6 +30,60 @@ end
 
 Capitalize.capitalize_name(Capitalize::Lambda_capitalize)
 
+#####SEM O MODULE######
+lambda_capitalize = ->(name) {puts name.capitalize}
+
+def self.capitalize_name(lambda_capitalize)
+  lambda_capitalize.call('andre')
+  lambda_capitalize.call('costa')
+end
+
+capitalize_name(lambda_capitalize)
+
+puts "----"
+#Missão 2
+
+# module Person
+#   class Juridic
+#     def initialize(name, cnpj)
+#       @name = name
+#       @cnpj = cnpj
+#     end
+#
+#     def add
+#     puts 'Pessoa Jurídica Aidcionada'
+#     puts "Nome: #{@name}"
+#     puts "cnpj: #{@cnpj}"
+#     end
+#   end
+# end
+#
+# Person::Juridic.new('M. C. Investimentos', '4241.123/0001').add
 
 
+#Missão 3
+require "cpf_cnpj"
+module Person
+  class Juridic
+    def initialize(name, cpf)
+      @name = name
+      @cpf = cpf
+    end
+
+    def add
+      puts "Nome e cpf do usuário"
+      puts "nome: #{@name}"
+      puts "Cpf: #{@cpf}"
+
+    if CPF.valid?(@cpf, strict: true)
+
+      puts "CPF Válido!"
+    elsif
+    puts "CPF Inválido"
+      end
+    end
+  end
+end
+
+Person::Juridic.new('André', '22222222222').add
 
