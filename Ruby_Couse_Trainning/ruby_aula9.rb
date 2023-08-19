c2ec99024da4c0d4923cfaad5d34bdfa1a2210b7
ded3v <decolacosta@gmail.com>
2023-08-19 15:40:29 -0300

    aula9 REGEX trainning

--- Ruby_Couse_Trainning/ruby_aula9.rb
+++ Ruby_Couse_Trainning/ruby_aula9.rb
@@ -1,1 +1,22 @@
+#REGEX - Validar entrada de dados ou fazer busca e extração
 
+text = "How are you doing? i'm fine, and looking to be awesome 1239!"
+find_data = /looking/.match(text)
+puts find_data
+puts find_data.pre_match
+puts find_data.post_match
+
+find_data = /[2-9]/.match(text)
+puts find_data
+
+text = "Contate joao@example.com, maria@test.com, ou joao@ex.com.br para mais informações."
+emails = text.scan(/\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/)
+puts emails
+
+puts
+
+puts "Capture os emails do texto:"
+
+texts = "asdfg@exemp.com, ou tbm qwer@hot.com.br, ou trewq@cresca.com"
+emails = texts.scan(/\b[A-Za-z0-9._%+]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}/)
+puts emails
