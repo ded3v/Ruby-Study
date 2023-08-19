b181ceef9f43808759cba6a9765e5b51a0c961ad
ded3v <decolacosta@gmail.com>
2023-08-18 21:24:13 -0300

    aula 8 trainning

--- Ruby_Couse_Trainning/ruby_aula8pt2.rb
+++ Ruby_Couse_Trainning/ruby_aula8pt2.rb
@@ -43,47 +43,93 @@ capitalize_name(lambda_capitalize)
 puts "----"
 #Missão 2
 
+module Person
+  class Juridic
+    def initialize(name, cnpj)
+      @name = name
+      @cnpj = cnpj
+    end
+
+    def add
+      puts 'Pessoa Jurídica Adicionada'
+      puts "Nome: #{@name}"
+      puts "cnpj: #{@cnpj}"
+    end
+  end
+
+    class Physical
+      def initialize(name, cpf)
+        @name = name
+        @cpf = cpf
+      end
+
+      def add
+        puts "Pessoa Física Adicionada"
+        puts "nome: #{@name}"
+        puts "Cpf: #{@cpf}"
+      end
+    end
+  end
+
+  Person::Juridic.new('M. C. Investimentos', '4241.123/0001').add
+puts"----"
+  Person::Physical.new('André Costa', '222.222.222-22').add
+
+#Mesmo código mas com verificação de cpf/cnpj
+  # require "cpf_cnpj"
 # module Person
-#   class Juridic
-#     def initialize(name, cnpj)
+#   class Physical
+#     def initialize(name, cpf)
 #       @name = name
-#       @cnpj = cnpj
+#       @cpf = cpf
 #     end
 #
 #     def add
-#     puts 'Pessoa Jurídica Aidcionada'
-#     puts "Nome: #{@name}"
-#     puts "cnpj: #{@cnpj}"
+#       puts "Nome e cpf do usuário"
+#       puts "nome: #{@name}"
+#       puts "Cpf: #{@cpf}"
+#
+#     if CPF.valid?(@cpf, strict: true)
+#
+#       puts "CPF Válido!"
+#     elsif
+#     puts "CPF Inválido"
+#       end
 #     end
 #   end
 # end
 #
-# Person::Juridic.new('M. C. Investimentos', '4241.123/0001').add
+# Person::Physical.new('André', '22222222222').add
 
+#MISSÃO 3
 
-#Missão 3
-require "cpf_cnpj"
-module Person
-  class Juridic
-    def initialize(name, cpf)
-      @name = name
-      @cpf = cpf
-    end
+#Proc:
+# Um Proc é um objeto que contém um bloco de código. Ele pode ser criado usando a classe Proc ou usando a sintaxe especial proc { ... }.
+# Aqui está um exemplo de como criar e usar um Proc:
+# my_proc = Proc.new { puts "Hello from Proc!" }
+# my_proc.call
 
-    def add
-      puts "Nome e cpf do usuário"
-      puts "nome: #{@name}"
-      puts "Cpf: #{@cpf}"
-
-    if CPF.valid?(@cpf, strict: true)
-
-      puts "CPF Válido!"
-    elsif
-    puts "CPF Inválido"
-      end
-    end
-  end
-end
-
-Person::Juridic.new('André', '22222222222').add
+#Diferenças notáveis do Proc:
+#
+# Flexibilidade de Argumentos: Um Proc é mais flexível em relação aos argumentos que recebe.
+# Se você chamar um Proc com mais ou menos argumentos do que o necessário, não causará um erro.
+#
+# Uso do return: O return dentro de um Proc faz com que o código retorne imediatamente do método onde o Proc foi chamado.
+#
+# Lambda:
+# Um lambda também é um objeto que encapsula um bloco de código, mas ele é mais estrito quanto a argumentos e ao uso do return.
+# Você pode criar um lambda usando a palavra-chave lambda ou usando a sintaxe -> { ... }.
+# Aqui está um exemplo de como criar e usar um lambda:
+# my_lambda = lambda { puts "Hello from Lambda!" }
+# my_lambda.call
+# Diferenças notáveis do lambda:
+#
+# Estrito em Relação a Argumentos: Um lambda é mais rigoroso em relação aos argumentos que recebe. Se você chamar um lambda com um número incorreto de argumentos, ele causará um erro.
+#
+# Comportamento do return: O return dentro de um lambda se comporta como se fosse dentro de um método, ou seja, ele retorna o valor do lambda sem sair do método que o chamou.
+#
+# Para resumir, a principal diferença entre Proc e lambda reside em como eles tratam os argumentos e o uso do return.
+# Em muitos casos, eles podem ser intercambiáveis, mas é importante entender as diferenças para evitar comportamentos inesperados.
+# Em geral, se você precisa de um bloco mais estrito com relação a argumentos e return, lambda é uma escolha mais segura.
+# Se você precisa de mais flexibilidade, Proc pode ser a melhor opção.
 
