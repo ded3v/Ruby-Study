c7627ea43e8c68faae68153a9a8bcf611f9bbaeb
ded3v <decolacosta@gmail.com>
2023-08-21 19:56:47 -0300

    exercício2

--- Ruby_Couse_Trainning/Exercícios_Ruby/1_condicionals.rb
+++ Ruby_Couse_Trainning/Exercícios_Ruby/1_condicionals.rb
@@ -30,3 +30,46 @@ when 35..39.9
 else
   puts "Obesidade grau 3"
 end
+
+#Exercício 2: Verificador de Número Primo
+# Crie um programa que solicita ao usuário um número inteiro positivo e verifica se ele é um número primo ou não.
+# Um número primo é aquele que só é divisível por 1 e por ele mesmo.
+
+num = 0
+puts "Digite um número para saber se é primo"
+num = gets.chomp.to_i
+while num <= 0
+  num = gets.chomp.to_i
+  puts "Número deve ser positivo e maior que 0"
+end
+
+k=0
+for i in 2..num # o i percorre assumindo os valores de 2 ao num
+  if num % i == 0
+    k = k + 1
+  end
+end
+if k == 1
+  puts "numero primo"
+  puts i
+else
+  puts "Número não é primo"
+end
+
+#método 2
+
+s_prime = true
+#Aqui o código fica mais enxuto, pois se leva em consideração calcular até o quadrado do número (pq após ele o quadro lógico se repete), diminuindo processamento
+for i in 2..Math.sqrt(num)
+  if num % i == 0
+    is_prime = false
+    break
+  end
+end
+
+if is_prime && num > 1
+  puts "Número primo"
+else
+  puts "Número não é primo"
+end
+
