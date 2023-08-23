b595bf71e4d0cfe2fa864923da8a2d174e0d4791
ded3v <decolacosta@gmail.com>
2023-08-23 13:15:50 -0300

    Exercício Random Number

--- Ruby_Couse_Trainning/Exercícios_Ruby/1_condicionals.rb
+++ Ruby_Couse_Trainning/Exercícios_Ruby/1_condicionals.rb
@@ -178,3 +178,22 @@ end
 
 puts"-----"
 
+####NÚMERO DA SORTE####
+
+puts "Digite um número de 0..100 e tente adivinhar o número da sorte!"
+number = gets.chomp.to_i
+
+random = rand(1..100)
+
+while number != random
+  if number < random
+    puts "O número ainda é maior que isso, tente novamente"
+  else
+    puts "Número é menor que isso, tente novamente"
+  end
+  number = gets.chomp.to_i
+end
+
+puts "Acertou! O número da sorte é #{random}"
+
+puts "-------"
