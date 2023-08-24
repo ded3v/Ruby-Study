dd8f8f74bfee90a498ec63060a738caa72bdb309
ded3v <decolacosta@gmail.com>
2023-08-24 17:09:57 -0300

    Matrix function for media calculation

--- Ruby_Course_Training/Exercícios_Ruby/funcoes_methods.rb
+++ Ruby_Course_Training/Exercícios_Ruby/funcoes_methods.rb
@@ -54,3 +54,36 @@ puts calc_media(notas1)
 
 puts "-------"
 
+#Método 2, usando uma matriz:
+
+# Defina os nomes das matérias e avaliações
+materias = ["Português", "Matemática", "Física"]
+avaliacoes = ["AV1", "AV2", "AV3"]
+
+# Crie a matriz de notas
+notas = []
+
+materias.each do |materia|
+  linha = []
+  avaliacoes.each do |avaliacao|
+    puts "Digite a nota de #{materia} - #{avaliacao}:"
+    nota = gets.chomp.to_f
+    linha << nota
+  end
+  notas << linha
+end
+
+# Imprime a matriz de notas
+puts "Matriz de Notas:"
+notas.each_with_index do |linha_notas, i|
+  puts "#{materias[i]}: #{linha_notas.join(', ')}"
+end
+
+# Calcula e imprime as médias
+puts "Médias:"
+notas.each_with_index do |linha_notas, i|
+  media = linha_notas.sum / avaliacoes.length
+  puts "#{materias[i]}: #{media}"
+end
+
+puts "--------"
