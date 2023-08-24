895bb306ac6050fef40b37bff8c8f238a3ec1a1f
ded3v <decolacosta@gmail.com>
2023-08-24 15:00:41 -0300

    using interesting function for media calculation

--- Ruby_Course_Training/Exercícios_Ruby/funcoes_methods.rb
+++ Ruby_Course_Training/Exercícios_Ruby/funcoes_methods.rb
@@ -7,11 +7,50 @@ end
 puts cheer('André')
 
 #ex2
+#Exercício 2: Calculadora Simples
 
-class Livro
-  def initialize(autor, isbn = 1 ,numero_paginas)
-    puts "Autor: #{autor} ISBN: #{isbn} Páginas: #{numero_paginas}"
+def calculadora(num1, operator, num2)
+  case operator
+  when "+"
+    return num1 + num2
+  when "-"
+    return num1 - num2
+  when "*"
+    return num1 * num2
+  when "/"
+    return num1 / num2
+  else
+    return "Operador inválido"
   end
 end
 
-Livro.new "Lucas Souza", 200
+
+puts "Digite o primeiro número:"
+number1 = gets.chomp.to_f
+
+puts "Digite o operador (+, -, *, /):"
+operator = gets.chomp
+
+puts "Digite o segundo número:"
+number2 = gets.chomp.to_f
+
+resultado = calculadora(number1, operator, number2)
+puts "Resultado: #{resultado}"
+
+
+##MEDIA_NOTAS
+
+def calc_media(notas)
+  return 0 if notas.empty?
+
+  total = notas.reduce(0.0) {|sum, nota| sum + nota} ## INteressante pontuar a função .reduce usado para somar termos de uma array por exemplo e sempre estruturado
+  # em pelo menos dois parâmetros, geralmente o primero adotado como 'sum' que vai acumular os valores
+  media = total/notas.length
+  return media
+end
+
+notas1 = [9, 8, 8.5, 10]
+puts calc_media(notas1)
+
+puts "-------"
+
