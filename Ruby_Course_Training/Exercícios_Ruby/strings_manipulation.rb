cca999089bcad3a8783d19b0ad9433c715de5f96
ded3v <decolacosta@gmail.com>
2023-08-25 20:52:16 -0300

    manipulating strings with classes

--- Ruby_Course_Training/Exercícios_Ruby/strings_manipulation.rb
+++ Ruby_Course_Training/Exercícios_Ruby/strings_manipulation.rb
@@ -2,7 +2,18 @@ class Saudation
   def hello(name)
     puts "Olá #{name}"
   end
+
+  def count(text)
+    text.length
+  end
 end
 
 greetings = Saudation.new
 puts greetings.hello("João")
+
+text = "Estou contando o número de caracteres"
+puts text
+puts "Número de caracteres #{greetings.count(text)}"
+
+puts "-------"
+
