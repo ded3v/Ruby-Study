823f52bfdea126fd604d6afb3f184ab0c5a57c61
ded3v <decolacosta@gmail.com>
2023-08-25 21:28:46 -0300

    adding reverse method

--- Ruby_Course_Training/Exercícios_Ruby/strings_manipulation.rb
+++ Ruby_Course_Training/Exercícios_Ruby/strings_manipulation.rb
@@ -6,6 +6,11 @@ class Saudation
   def count(text)
     text.length
   end
+
+  def reversed(text)
+    puts text.reverse
+
+  end
 end
 
 greetings = Saudation.new
@@ -14,6 +19,6 @@ puts greetings.hello("João")
 text = "Estou contando o número de caracteres"
 puts text
 puts "Número de caracteres #{greetings.count(text)}"
-
+puts "Frase reversa: #{greetings.reversed(text)}"
 puts "-------"
 
