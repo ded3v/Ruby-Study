8d10c7a3b71a709ff774578d65db15021c6b2372
ded3v <decolacosta@gmail.com>
2023-08-27 14:20:51 -0300

    sentence capitalize

--- Ruby_Course_Training/Exercícios_Ruby/strings_manipulation.rb
+++ Ruby_Course_Training/Exercícios_Ruby/strings_manipulation.rb
@@ -22,3 +22,23 @@ puts "Número de caracteres #{greetings.count(text)}"
 puts "Frase reversa: #{greetings.reversed(text)}"
 puts "-------"
 
+
+#Exercício 4: Corretor de Capitalização
+# Desenvolva um programa que recebe uma frase do usuário, mas todas as letras estão em caixa alta.
+# O programa deve corrigir a capitalização para que apenas a primeira letra de cada sentença seja maiúscula, enquanto o restante da frase é minúsculo.
+
+def sentence_capitalize(sentence)
+  words = sentence.split('. ')
+  correct = []
+
+  words.each do |sentenc|
+    correct << sentenc.capitalize
+  end
+  correct.join('. ')
+end
+
+puts "Digite uma frase em caixa alta"
+sentence = gets.chomp
+
+puts "Frase corrigida:"
+puts sentence_capitalize(sentence)
