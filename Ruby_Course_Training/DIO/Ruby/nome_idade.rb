914522a3e7880a6002342796a10fa64b7d6e506f
ded3v <decolacosta@gmail.com>
2023-08-27 14:36:49 -0300

    dio task

---
+++ Ruby_Course_Training/DIO/Ruby/nome_idade.rb
@@ -0,0 +1,8 @@
+puts "Digite seu nome: "
+nome = gets.chomp
+puts "Digite seu sobrenome: "
+sobrenome = gets.chomp
+puts "Digite sua idade:"
+idade = gets.chomp
+
+puts "Nome completo: #{nome} #{sobrenome}, idade: #{idade}"
