53a0299f83abfd6488367289209fdc515ced3139
ded3v <decolacosta@gmail.com>
2023-08-31 16:39:42 -0300

    Adding CLass_Object exercise

--- Ruby_Course_Training/Exercícios_Ruby/Class_Object.rb
+++ Ruby_Course_Training/Exercícios_Ruby/Class_Object.rb
@@ -1,3 +1,6 @@
+#Exercício 1: Classe Pessoa
+# Crie uma classe chamada Pessoa que tenha os atributos nome e idade.
+# Crie um objeto dessa classe e atribua valores aos atributos. Em seguida, crie um método que exiba uma mensagem contendo o nome e a idade da pessoa.
 class Person
     attr_accessor :name, :age
 
@@ -11,4 +14,19 @@ person.name = "André"
 person.age = 31
 person.hello
 
+#Exercício 2: Classe Cachorro
+# Crie uma classe chamada Cachorro que tenha os atributos nome e raca.
+# Crie um objeto dessa classe e atribua valores aos atributos. Em seguida, crie um método que exiba uma mensagem contendo o nome e a raça do cachorro.
 
+class Dog
+  attr_accessor :name, :race
+
+  def name_race
+    puts "O nome deste cachorro é #{@name}, da raça #{@race}, e é um animal belíssimo"
+  end
+end
+
+dog = Dog.new
+dog.name = "Ury"
+dog.race = "Rottweiler"
+dog.name_race
