1c3d32ab876c82e24acf907ab3a02d812a0ebf12
ded3v <decolacosta@gmail.com>
2023-09-02 23:55:59 -0300

    DIo REgex

--- Ruby_Course_Training/DIO/Ruby/regular_expressions.rb
+++ Ruby_Course_Training/DIO/Ruby/regular_expressions.rb
@@ -8,3 +8,28 @@ find_data = /looking/.match(text)
 puts find_data
 puts find_data.pre_match
 puts find_data.post_match
+
+find_data = /[2-9]/.match(text)
+puts find_data
+
+text = "Contate joao@example.com, maria@test.com, ou joao@ex.com.br para mais informações."
+emails = text.scan(/\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/)
+puts emails
+
+puts
+
+
+puts Math.sqrt(8100)
+puts Math.log10(100)
+puts radian = 30 * (Math::PI / 180)
+puts Math::E
+puts Math::PI
+
+#TIME
+time = Time.now
+puts time
+
+puts time.strftime('%d/%m/%y')
+
+puts time.saturday?
+
