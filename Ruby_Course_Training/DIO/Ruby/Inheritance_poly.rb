9840c98b178c4cbb7f6f307edfaa72685db97dba
ded3v <decolacosta@gmail.com>
2023-09-08 22:39:25 -0300

    Ingeritance exercises 2

--- Ruby_Course_Training/DIO/Ruby/Inheritance_poly.rb
+++ Ruby_Course_Training/DIO/Ruby/Inheritance_poly.rb
@@ -1,12 +1,31 @@
 class Vehicle
   attr_accessor :brand, :model, :year
+
+  def initialize(brand, model, year)
+    @brand = brand
+    @model = model
+    @year = year
+  end
 end
 
 class Car < Vehicle
   attr_accessor :door, :color
+
+  def initialize(brand, model, year, door, color)
+    super(brand, model, year)
+    @door = door
+    @color = color
+  end
 end
 
 class Motorcycle < Vehicle
   attr_accessor :displacement, :color
+
+  def initialize(brand, model, year, displacement, color)
+    super(brand, model, year)
+    @displacement = displacement
+    @color = color
+  end
 end
 
+car = Car.new('Voyage', )
