276f10a1ebc0dc935306db63e5ceaaaba6727131
ded3v <decolacosta@gmail.com>
2023-09-10 20:41:22 -0300

    manually creating other routes

--- Rails/Rails2/mvc_test/config/routes.rb
+++ Rails/Rails2/mvc_test/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do  #Aqui onde tudo começa. É necessário a permissão de uma rota para acesso ao site destino
-  resources :users
+  #resources :users
   get 'userslist', to: 'users#index'
+  post 'create', to: 'users#create'
+  get 'new', to: 'users#new'
+  get 'edit', to: 'users#edit'
+  get 'show', to: 'users#show'
+  patch 'update', to: 'users#update'
+  delete 'delete', to: 'users#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
